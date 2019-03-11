05756bcd29445a83372b775b575a4e53a95081ed
alankost <akost@arizona.edu>
2019-03-10 19:31:59 -0700

    Upload Themes

---
+++ Themes2/_standard.py
@@ -0,0 +1,86 @@
+#-----------------------------------------------------------------------------
+# Copyright (c) 2012 - 2019, Anaconda, Inc., and Bokeh Contributors.
+# All rights reserved.
+#
+# The full license is in the file LICENSE.txt, distributed with this software.
+#-----------------------------------------------------------------------------
+
+json = {
+    "attrs": {
+        "Figure": {
+            "plot_width": 400,
+            "plot_height": 400,
+            "background_fill_color": "lightgray",
+            "border_fill_color": "white",
+            "outline_line_color": "black",
+            "outline_line_alpha": 1.0,
+            "outline_line_width": 1.0
+        },
+
+        "Grid": {
+            "grid_line_color": "white",
+            "grid_line_alpha": 0.6
+        },
+
+        "Axis": {
+            "major_tick_line_alpha": 1,
+            "major_tick_line_color": "black",
+
+            "minor_tick_line_alpha": 1,
+            "minor_tick_line_color": "black",
+
+            "axis_line_alpha": 0,
+            "axis_line_color": "#E0E0E0",
+
+            "major_label_text_color": "black",
+            "major_label_text_font": "Arial",
+            "major_label_text_font_size": "12pt",
+
+            "axis_label_standoff": 10,
+            "axis_label_text_color": "black",
+            "axis_label_text_font": "Arial",
+            "axis_label_text_font_size": "12pt",
+            "axis_label_text_font_style": "normal"
+        },
+
+        "Legend": {
+            "spacing": 8,
+            "glyph_width": 15,
+
+            "label_standoff": 8,
+            "label_text_color": "black",
+            "label_text_font": "Arial",
+            "label_text_font_size": "12pt",
+
+            "border_line_alpha": 0,
+            "background_fill_alpha": 0.25,
+            "background_fill_color": "white"
+        },
+
+        "ColorBar": {
+            "title_text_color": "black",
+            "title_text_font": "Arial",
+            "title_text_font_size": "12pt",
+            "title_text_font_style": "normal",
+
+            "major_label_text_color": "black",
+            "major_label_text_font": "Arial",
+            "major_label_text_font_size": "12pt",
+
+            "background_fill_color": "#15191C",
+            "major_tick_line_alpha": 0,
+            "bar_line_alpha": 0
+        },
+
+        "Title": {
+            "text_color": "black",
+            "text_font": "Arial",
+            "text_font_size": "12pt",
+            "text_font_style": "normal"
+        },
+        
+        "Line": {
+            "line_color": "darkblue"
+        }
+    }
+}
